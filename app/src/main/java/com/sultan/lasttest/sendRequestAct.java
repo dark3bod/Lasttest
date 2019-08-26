3edcdbd0b672606386fbf5e1e6b8ec653a5bc33a
Sultan <su0ltan@hotmail.com>
2019-08-26 14:20:56 +0300

    Add sendRequestAct Activity and manage the Design

---
+++ app/src/main/java/com/sultan/lasttest/sendRequestAct.java
@@ -0,0 +1,14 @@
+package com.sultan.lasttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sendRequestAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_request);
+    }
+}
