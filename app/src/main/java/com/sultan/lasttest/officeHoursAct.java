58e6ed1dda0570ec144d4ef676b5784f5ec3801a
Sultan <su0ltan@hotmail.com>
2019-09-22 18:24:33 +0300

    add Office hours Activity

---
+++ app/src/main/java/com/sultan/lasttest/officeHoursAct.java
@@ -0,0 +1,14 @@
+package com.sultan.lasttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class officeHoursAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_office_hours);
+    }
+}
